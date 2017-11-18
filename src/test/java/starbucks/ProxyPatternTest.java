34969e7c87b1bb4db52c71c9995535d21d89e75b
Dhanashree Gaonkar <dgaonkar17@gmail.com>
2017-11-18 14:34:06 -0800

    update

--- src/test/java/starbucks/ProxyPatternTest.java
+++ src/test/java/starbucks/ProxyPatternTest.java
@@ -43,10 +43,14 @@ public class ProxyPatternTest
     {
         // Test Successful Pin:  
         // (enter correct pin digits)
+  app.touch(1,5);
+        app.touch(2,5);
+        app.touch(3,5);
+        app.touch(4,5);
 
         // Validate Pin Succeeded 
         // (replace with correct assert)
-        assertTrue( false ) ;
+        assertTrue( app.screen(),"MyCards" ) ;
     }
 
     @Test
@@ -54,10 +58,14 @@ public class ProxyPatternTest
     {
         // Test Failed Pin:  
         // (enter incorrect pin digits)
+  app.touch(1,5);
+        app.touch(2,5);
+        app.touch(3,5);
+        app.touch(3,5);
 
         // Validate Pin Succeeded 
         // (replace with correct assert)
-        assertTrue( false ) ;
+        assertTrue(app.screen(),"PinScreen") ;
     }
 
     
